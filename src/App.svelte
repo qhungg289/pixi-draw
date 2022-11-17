94e8c6dba4381363b7794dbf915d43bab7abe6aa
Hung Duong Quang <qhungg289@protonmail.com>
2022-11-17 10:39:32 +0700

    update App

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
+	import TitleBar from "./lib/TitleBar.svelte";
+	import Palette from "./lib/Palette.svelte";
+	import CurrentColor from "./lib/CurrentColor.svelte";
+
 	import { onMount } from "svelte";
 
-	const PIXEL_SIZE = 20;
+	const PIXEL_SIZE = 40;
 	const PALETTE = [
 		"000000",
 		"1D2B53",
@@ -22,29 +26,66 @@
 	];
 
 	let layer = [];
-	let dimension = { width: 16, height: 16 };
-	let selectedColor = PALETTE[7];
+	let dimension = { width: 8, height: 8 };
+	let selectedColor = PALETTE[8];
 	let canvas;
 
-	onMount(() => {
+	function draw() {
 		const ctx = canvas.getContext("2d");
 
 		for (let i = 0; i < dimension.height; i++) {
-			for (let j = 0; j < dimension.width; j++) {}
+			for (let j = 0; j < dimension.width; j++) {
+				const x = i * (canvas.width / dimension.width);
+				const y = j * (canvas.height / dimension.height);
+
+				ctx.fillStyle = layer[i][j];
+				ctx.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+			}
 		}
+	}
+
+	onMount(() => {
+		layer = new Array(dimension.height);
+		for (let i = 0; i < dimension.width; i++) {
+			layer[i] = new Array(dimension.width);
+		}
+
+		for (let i = 0; i < dimension.height; i++) {
+			for (let j = 0; j < dimension.width; j++) {
+				layer[i][j] = PALETTE[0];
+			}
+		}
+
+		draw();
 	});
+
+	$: console.table(layer);
 </script>
 
-<div>
-	<h1>Pixi Draw</h1>
-</div>
+<TitleBar />
 
-<div>
+<div class="flex items-center justify-center h-4/6">
 	<canvas
 		bind:this={canvas}
 		width={dimension.width * PIXEL_SIZE}
 		height={dimension.height * PIXEL_SIZE}
+		on:click={(e) => {
+			const rect = canvas.getBoundingClientRect();
+			const scaleX = canvas.width / rect.width;
+			const scaleY = canvas.height / rect.height;
+			const x = Math.floor(((e.clientX - rect.left) * scaleX) / PIXEL_SIZE);
+			const y = Math.floor(((e.clientY - rect.top) * scaleY) / PIXEL_SIZE);
+			layer[y][x] = selectedColor;
+			draw();
+		}}
 	/>
 </div>
 
-<div />
+<div class="border-t-2">
+	<CurrentColor {selectedColor} />
+
+	<Palette
+		palette={PALETTE}
+		on:colorchange={(e) => (selectedColor = e.detail)}
+	/>
+</div>
