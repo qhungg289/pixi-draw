68c100b1235a306923e6a8e75334742da8b3d256
Hung Duong Quang <qhungg289@protonmail.com>
2022-11-17 09:43:17 +0700

    add Nunito font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		fontFamily: {
+			nunito: ["Nunito", "sans-serif"],
+		},
 	},
 	plugins: [],
 };
